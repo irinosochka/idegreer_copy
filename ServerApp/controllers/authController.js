0ce8ed14eb7961da88d8a962b85806d2cde2aebf
vadym.hvozditskyi <vadym.hvozditskyi@onwelo.com>
2022-03-16 11:34:47 +0100

    Server files refactoring

--- ServerApp/controllers/authController.js
+++ ServerApp/controllers/authController.js
@@ -13,16 +13,6 @@ class AuthController {
         }
     }
 
-    async userDataChanging(req, res) {
-        try {
-            const {username, password, newUsername, newName, newEmail} = req.body;
-            const data = await authService.userDataChanging(username, password, newUsername, newName, newEmail);
-            return res.json({data, resultCode: 1})
-        } catch(e) {
-            return res.json({message: "Login error", resultCode: 0})
-        }
-    }
-
     async registration(req, res) {
         try {
             const errors = validationResult(req);
@@ -58,16 +48,6 @@ class AuthController {
             return res.json(token);
         } catch(e) {
             res.status(505).json({message: "Logout error " + e})
-        }
-    }
-
-    async passwordChanging(req, res) {
-        try {
-            const {username, password, newPassword} = req.body;
-            const data = await authService.passwordChanging(username, password, newPassword);
-            return res.json({data, resultCode: 1})
-        } catch(e) {
-            res.status(505).json({message: "Password changing error", resultCode: 0})
         }
     }
 
