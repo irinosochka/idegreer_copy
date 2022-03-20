c3ae1f28e5fb19a8c0dcd0950caad568052ae597
Oleksandr Matushevskyi <oleksandr.matushevskyi@gmail.com>
2022-03-20 20:10:48 +0100

    Ability to add the price of the course

--- ClientApp/src/services/CourseService.ts
+++ ClientApp/src/services/CourseService.ts
@@ -2,8 +2,8 @@ import {$course} from "../http";
 import {CourseResponse} from "../models/response/CourseResponse";
 
 export default class CourseService {
-    static addCourse(username: string, title: string, theme: string, description: string): Promise<any> {
-        return $course.post<CourseResponse>('/add', {username, title, theme, description}).then(res => {
+    static addCourse(username: string, title: string, theme: string, description: string, price: string): Promise<any> {
+        return $course.post<CourseResponse>('/add', {username, title, theme, description, price}).then(res => {
             return res
         }).catch(e => console.log(e))
     }
