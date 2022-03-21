c730d1ce1bb5366486534f30229c5ca00d42d173
vadym.hvozditskyi <vadym.hvozditskyi@onwelo.com>
2022-03-21 20:50:41 +0100

    Registration but refactoring

--- ServerApp/models/user-model.js
+++ ServerApp/models/user-model.js
@@ -1,7 +1,6 @@
 const {Schema, model} = require("mongoose");
 
 const UserModel = new Schema({
-    _id: Schema.Types.ObjectId,
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     name: {type: String, required: true},
