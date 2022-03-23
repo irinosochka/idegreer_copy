8214f564fefcab4481f9ee4014f068fbe1585967
vadym.hvozditskyi <vadym.hvozditskyi@onwelo.com>
2022-03-23 23:17:47 +0100

    Profile button styles refactoring

--- ClientApp/src/components/EditProfile/EditProfileButton.tsx
+++ ClientApp/src/components/EditProfile/EditProfileButton.tsx
@@ -1,19 +1,17 @@
 import React, {FC} from 'react';
 // @ts-ignore
 import profileIcon from "../../assets/img/user-profile-svgrepo-com.svg";
-import {UserPageSlidesItems} from "../../pages/UserPage";
 
 interface EditProfileButtonProps {
     icon: string,
-    slideItem: UserPageSlidesItems,
     onClick: () => any,
     isActive: boolean
 }
 
-const EditProfileButton: FC<EditProfileButtonProps> = ({icon, isActive, slideItem, onClick, children}) => {
+const EditProfileButton: FC<EditProfileButtonProps> = ({icon, isActive, onClick, children}) => {
     return (
         <div className={`edit__btn ${isActive && 'active'}`}
-             style={{display: 'flex', alignItems: 'center', marginTop: '30px'}}>
+             style={{display: 'flex', alignItems: 'center', marginTop: '30px', cursor: 'pointer'}}  onClick={onClick}>
             <div style={{
                 display: 'flex',
                 alignItems: 'center',
@@ -24,7 +22,7 @@ const EditProfileButton: FC<EditProfileButtonProps> = ({icon, isActive, slideIte
             }}>
                 <img src={icon} style={{width: '20px', height: '20px'}} alt=""/>
             </div>
-            <button onClick={onClick}>{children}</button>
+            <button>{children}</button>
         </div>
     );
 };
