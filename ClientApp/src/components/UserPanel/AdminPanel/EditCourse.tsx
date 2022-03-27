0ce9dc26f0dda34c90eb31171b8a143e5c1a6595
vadym.hvozditskyi <vadym.hvozditskyi@onwelo.com>
2022-03-27 23:33:51 +0200

    edit course input value refactoring

--- ClientApp/src/components/UserPanel/AdminPanel/EditCourse.tsx
+++ ClientApp/src/components/UserPanel/AdminPanel/EditCourse.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {AppStateType} from "../../../reduxStore/store";
 import {connect} from "react-redux";
 import {getAllCourses} from "../../../reduxStore/course-reducer";
--- ClientApp/src/components/UserPanel/AdminPanel/EditCourseForm.tsx
+++ ClientApp/src/components/UserPanel/AdminPanel/EditCourseForm.tsx
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {AppStateType} from "../../../reduxStore/store";
 import {connect} from "react-redux";
-import {changeCourseData, getAllCourses} from "../../../reduxStore/course-reducer";
+import {actions, changeCourseData, getAllCourses} from "../../../reduxStore/course-reducer";
 import {ICourse} from "../../../models/ICourse";
 import "./adminPanel.css";
 import Message, {MessageType} from "../../../common/Messages/Message";
 import Button from "../../../common/button/Button";
-import {actions} from "../../../reduxStore/course-reducer";
 
 
 interface EditCourseProps {
@@ -24,12 +23,6 @@ const EditCourseForm: React.FC<EditCourseProps> = ({course, changeCourseData, co
 
     const [isError, setError] = useState(false);
 
-    useEffect(() => {
-        setTitle(course.title);
-        setTheme(course.theme);
-        setPrice(course.price);
-        setDescription(course.description);
-    },[]);
 
     const handleSubmit = (event: any) => {
         event.preventDefault();
@@ -43,63 +36,64 @@ const EditCourseForm: React.FC<EditCourseProps> = ({course, changeCourseData, co
     };
 
     return (
-            <div style={{width: '400px', marginTop: '130px'}}>
-                {isError && <Message type={MessageType.ERROR}>Fields can't be empty</Message>}
-                {courseDataChangedSuccess && <Message type={MessageType.SUCCESS}>Success data changing</Message>}
+        <div style={{width: '400px', marginTop: '130px'}}>
+            {isError && <Message type={MessageType.ERROR}>Fields can't be empty</Message>}
+            {courseDataChangedSuccess && <Message type={MessageType.SUCCESS}>Success data changing</Message>}
 
-                <form onSubmit={handleSubmit}>
-                    <input
-                        onChange={(event)=> {
-                            setTitle(event.target.value);
-                            setError(false);
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(event) => {
+                        setTitle(event.target.value);
+                        setError(false);
 
 
-                        }}
-                        type="text"
-                        id="course_name"
-                        value={title}
-                        placeholder={course.title}
-                    />
-                    <input
-                        onChange={(event)=> {
-                            setTheme(event.target.value);
-                            setError(false);
-                        }}
-                        type="text"
-                        id="course_topic"
-                        value={theme}
-                        placeholder={course.theme}
-                    />
-                    <input
-                        onChange={(event)=> {
-                            setPrice(event.target.value);
-                            setError(false);
-                        }}
-                        type="number"
-                        id="course_price"
-                        value={price}
-                        placeholder={course.price}
-                    />
-                    <textarea
-                        onChange={(event)=> {
-                            setDescription(event.target.value);
-                            setError(false);
-                        }}
-                        value={description}
-                        name="textarea"
-                        id="course_description"
-                        placeholder={course.description}
-                        style={{resize: "none", marginBottom: '10px', padding: '5px 15px', width: 'calc(100% - 32px)'}}
-                    />
-                    <p style={{color: 'slategrey'}}>Author: {course.author.name}</p>
-                    <div>
+                    }}
+                    type="text"
+                    id="course_name"
+                    value={title}
+                    placeholder={course.title}
+                />
+                <input
+                    onChange={(event) => {
+                        setTheme(event.target.value);
+                        setError(false);
+                    }}
+                    type="text"
+                    id="course_topic"
+                    value={theme}
+                    placeholder={course.theme}
+                />
+                <input
+                    onChange={(event) => {
+                        setPrice(event.target.value);
+                        setError(false);
+                    }}
+                    type="number"
+                    id="course_price"
+                    value={price}
+                    placeholder={course.price}
+                />
+                <textarea
+                    onChange={(event) => {
+                        setDescription(event.target.value);
+                        setError(false);
+                    }}
+                    value={description}
+                    name="textarea"
+                    id="course_description"
+                    placeholder={course.description}
+                    style={{resize: "none", marginBottom: '10px', padding: '5px 15px', width: 'calc(100% - 32px)'}}
+                />
+                <p style={{color: 'slategrey'}}>Author: {course.author.name}</p>
+                <div>
 
-                        <Button width={240}>Submit changes</Button>
-                    </div>
-                </form>
-            </div>
+                    <Button width={240}>Submit changes</Button>
+                </div>
+            </form>
+        </div>
 
-    )}
+    )
+}
 
 const mapStateToProps = (state: AppStateType) => {
     return {
