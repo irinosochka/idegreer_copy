24585765fe9299e7f7cb7d25d5de9f8114ace527
vadym.hvozditskyi <vadym.hvozditskyi@onwelo.com>
2022-05-31 16:34:40 +0200

    lection loading after first page opening

--- ClientApp/src/components/CoursePageComponets/UserCourse.tsx
+++ ClientApp/src/components/CoursePageComponets/UserCourse.tsx
@@ -1,12 +1,16 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {connect} from "react-redux";
 import {actions as authActions} from "../../reduxStore/auth-reducer";
-import {actions as lectionActions, addHomeworkResponse} from "../../reduxStore/lection-reducer";
+import {
+    actions as lectionActions,
+    addHomeworkResponse,
+    getAllLectionsFromCourse
+} from "../../reduxStore/lection-reducer";
 import YouTube from "react-youtube";
 import {ICourse} from "../../models/ICourse";
 import {ILection} from "../../models/ILection";
 import {AppStateType} from "../../reduxStore/store";
-import {addUserToCourse} from "../../reduxStore/course-reducer";
+import {addUserToCourse, getOneCourse} from "../../reduxStore/course-reducer";
 import PhotoMockup, {sizeTypes} from "../../common/photoMockup/PhotoMockup";
 
 import backIcon from "../../assets/img/back-svgrepo-com.svg"
@@ -23,21 +27,32 @@ interface UserCourseProps {
     course: { course: ICourse, author: IUser },
     setLection: () => void,
     lections: ILection[],
-    addHomeworkResponse: (userId: string, courseId: string, lectionId: string, resp: string) => void
+    addHomeworkResponse: (userId: string, courseId: string, lectionId: string, resp: string) => void,
+    getAllLectionsFromCourse: (courseId: string) => void,
+    getOneCourse: (courseId: string) => void,
 }
 
 const UserCourse: FC<UserCourseProps> = ({
-                                                 course,
-                                                 authUser,
-                                                 lections,
-                                                 addHomeworkResponse
-                                             }) => {
+                                             course,
+                                             authUser,
+                                             lections,
+                                             addHomeworkResponse,
+                                             getAllLectionsFromCourse,
+                                             getOneCourse
+                                         }) => {
     const [activeLection, setActiveLection] = useState<ILection | null>(lections[0]);
     const [homeworkText, setHomeworkText] = useState('');
     const [isError, setError] = useState(false);
     const [sentHomework, setSentHomework] = useState(false);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (course) {
+            getOneCourse(course.course._id)
+            getAllLectionsFromCourse(course.course._id);
+        }
+    }, [])
 
     const handleClose = (event: React.FormEvent) => {
         event.preventDefault();
@@ -83,9 +98,9 @@ const UserCourse: FC<UserCourseProps> = ({
                     {/*<PhotoMockup size={sizeTypes.small}/>*/}
 
                     <div className='professor__icon'>
-                            <h2 style={{margin: 'auto', color: '#e6ebff', fontWeight: 400}}>
-                                {initial()}
-                            </h2>
+                        <h2 style={{margin: 'auto', color: '#e6ebff', fontWeight: 400}}>
+                            {initial()}
+                        </h2>
                     </div>
 
                     <div style={{marginLeft: '10px'}}>
@@ -106,39 +121,39 @@ const UserCourse: FC<UserCourseProps> = ({
 
             <div className="lecture-player__container">
                 {activeLection &&
-                <div>
-                    <h2 style={{textAlign: 'center'}}>{activeLection.title}</h2>
-                    <div style={{marginBottom: '20px'}}>
-                        <YouTube videoId={activeLection?.link}/>
-                    </div>
-
-                    <hr/>
-                    <div className="homework">
-                        <div className='professor__icon' style={{marginRight: '10px'}}>
-                            <h2 style={{margin: 'auto', color: '#e6ebff', fontWeight: 400}}>
-                                {initial()}
-                            </h2>
+                    <div>
+                        <h2 style={{textAlign: 'center'}}>{activeLection.title}</h2>
+                        <div style={{marginBottom: '20px'}}>
+                            <YouTube videoId={activeLection?.link}/>
                         </div>
 
-                        <div>
-                            {course.author &&
-                            <h3>
-                                {course.author.name}
-                            </h3>}
-                            <p>
-                                {activeLection.homework}
-                            </p>
-                        </div>
-                    </div>
+                        <hr/>
+                        <div className="homework">
+                            <div className='professor__icon' style={{marginRight: '10px'}}>
+                                <h2 style={{margin: 'auto', color: '#e6ebff', fontWeight: 400}}>
+                                    {initial()}
+                                </h2>
+                            </div>
 
-                    <div className="add-homework">
-                        <div style={{marginRight: '10px'}}>
-                            <PhotoMockup size={sizeTypes.small}/>
+                            <div>
+                                {course.author &&
+                                    <h3>
+                                        {course.author.name}
+                                    </h3>}
+                                <p>
+                                    {activeLection.homework}
+                                </p>
+                            </div>
                         </div>
-                        { !sentHomework &&
-                        <form onSubmit={onHomeworkSubmit}>
-                            {isError && <Message type={MessageType.ERROR}>Fields can't be empty</Message>}
-                            <div className="input-wrapper" style={{marginTop: '10px'}}>
+
+                        <div className="add-homework">
+                            <div style={{marginRight: '10px'}}>
+                                <PhotoMockup size={sizeTypes.small}/>
+                            </div>
+                            {!sentHomework &&
+                                <form onSubmit={onHomeworkSubmit}>
+                                    {isError && <Message type={MessageType.ERROR}>Fields can't be empty</Message>}
+                                    <div className="input-wrapper" style={{marginTop: '10px'}}>
                                      <textarea className="form-control"
                                                onChange={(event) => {
                                                    setHomeworkText(event.target.value)
@@ -148,17 +163,17 @@ const UserCourse: FC<UserCourseProps> = ({
                                                id="homework_text"
                                                placeholder='Enter your homework'
                                      />
-                            </div>
-                            <Button>Submit</Button>
-                        </form>
-                        }
-                        { sentHomework &&
-                            <div className="message-sentHomework" style={{marginTop: '10px'}}>
-                                Homework was sent
-                            </div>
-                        }
-                    </div>
-                </div>}
+                                    </div>
+                                    <Button>Submit</Button>
+                                </form>
+                            }
+                            {sentHomework &&
+                                <div className="message-sentHomework" style={{marginTop: '10px'}}>
+                                    Homework was sent
+                                </div>
+                            }
+                        </div>
+                    </div>}
             </div>
         </div>
     );
@@ -179,5 +194,7 @@ export default connect(mapStateToProps, {
     setLection: lectionActions.setLections,
     setLoading: authActions.setLoading,
     addUserToCourse,
-    addHomeworkResponse
+    addHomeworkResponse,
+    getAllLectionsFromCourse,
+    getOneCourse
 })(UserCourse);
