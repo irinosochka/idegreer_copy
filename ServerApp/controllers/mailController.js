78a82e6f14e2e72fc6d5fd66122637016399067e
vadym.hvozditskyi <vadym.hvozditskyi@onwelo.com>
2022-06-07 11:11:50 +0200

    system bug fixing

--- ServerApp/controllers/mailController.js
+++ ServerApp/controllers/mailController.js
@@ -21,19 +21,21 @@ class MailController {
         });
 
         const usersIds = await CourseService.getAllUsersFromCourse(courseId);
-        const userArr = []
-        for (const user of usersIds.userList) {
-            await UserService.getUserUsingId(user).then(res => userArr.push(res.user));
-        }
-        let mailList = [];
-        userArr.forEach(user => mailList.push(user.email))
+        if (usersIds.length > 0) {
 
-        const message = {
-            from: `IDegreer <${process.env.EMAIL}>`, // sender address
-            to: mailList, // list of receivers
-            subject: 'IDegreer updates', // Subject line
-            text: userMessage, // plain text body
-            html: `<body style="width: 500px;
+            const userArr = []
+            for (const user of usersIds.userList) {
+                await UserService.getUserUsingId(user).then(res => userArr.push(res.user));
+            }
+            let mailList = [];
+            userArr.forEach(user => mailList.push(user.email))
+
+            const message = {
+                from: `IDegreer <${process.env.EMAIL}>`, // sender address
+                to: mailList, // list of receivers
+                subject: 'IDegreer updates', // Subject line
+                text: userMessage, // plain text body
+                html: `<body style="width: 500px;
                       margin-right: 10px;
                       font-family: BlinkMacSystemFont, 'Helvetica Neue', sans-serif;
                       text-align: center">
@@ -54,21 +56,22 @@ class MailController {
                         <h3 style="font-weight: 500; color: #000">Thanks for using iDegreer</h3>
                       </div>
                     </body>`
-        }
-
-        transporter.sendMail(message, (err, info) => {
-            if(err) {
-                console.log("error in sending mail", err);
-                return res.status(400).json({
-                    message: `error in sending mail ${err}`
-                })
-            } else {
-                console.log("successfully send the mail", info)
-                return res.json({
-                    message: info
-                })
             }
-        })
+
+            transporter.sendMail(message, (err, info) => {
+                if(err) {
+                    console.log("error in sending mail", err);
+                    return res.status(400).json({
+                        message: `error in sending mail ${err}`
+                    })
+                } else {
+                    console.log("successfully send the mail", info)
+                    return res.json({
+                        message: info
+                    })
+                }
+            })
+        }
     }
 }
 
